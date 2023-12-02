1ff4319f2a8f77af68c96ea8f6bc26627d3788dc
jiefong <jiefonggan@gmail.com>
2023-12-03 02:19:34 +0800

    main page

---
+++ about.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>About - AllHere</title>
+  <meta property="og:title" content="About - AllHere" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="utf-8" />
+  <meta property="twitter:card" content="summary_large_image" />
+
+  <style data-tag="reset-style-sheet">
+    html {
+      line-height: 1.15;
+    }
+
+    body {
+      margin: 0;
+    }
+
+    * {
+      box-sizing: border-box;
+      border-width: 0;
+      border-style: solid;
+    }
+
+    p,
+    li,
+    ul,
+    pre,
+    div,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    figure,
+    blockquote,
+    figcaption {
+      margin: 0;
+      padding: 0;
+    }
+
+    button {
+      background-color: transparent;
+    }
+
+    button,
+    input,
+    optgroup,
+    select,
+    textarea {
+      font-family: inherit;
+      font-size: 100%;
+      line-height: 1.15;
+      margin: 0;
+    }
+
+    button,
+    select {
+      text-transform: none;
+    }
+
+    button,
+    [type="button"],
+    [type="reset"],
+    [type="submit"] {
+      -webkit-appearance: button;
+    }
+
+    button::-moz-focus-inner,
+    [type="button"]::-moz-focus-inner,
+    [type="reset"]::-moz-focus-inner,
+    [type="submit"]::-moz-focus-inner {
+      border-style: none;
+      padding: 0;
+    }
+
+    button:-moz-focus,
+    [type="button"]:-moz-focus,
+    [type="reset"]:-moz-focus,
+    [type="submit"]:-moz-focus {
+      outline: 1px dotted ButtonText;
+    }
+
+    a {
+      color: inherit;
+      text-decoration: inherit;
+    }
+
+    input {
+      padding: 2px 4px;
+    }
+
+    img {
+      display: block;
+    }
+
+    html {
+      scroll-behavior: smooth
+    }
+  </style>
+  <style data-tag="default-style-sheet">
+    html {
+      font-family: Inter;
+      font-size: 16px;
+    }
+
+    body {
+      font-weight: 400;
+      font-style: normal;
+      text-decoration: none;
+      text-transform: none;
+      letter-spacing: normal;
+      line-height: 1.15;
+      color: var(--dl-color-gray-black);
+      background-color: var(--dl-color-gray-white);
+
+    }
+  </style>
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&amp;display=swap"
+    data-tag="font" />
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+    data-tag="font" />
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+    data-tag="font" />
+  <style>
+    @keyframes fade-in-left {
+      0% {
+        opacity: 0;
+        transform: translateX(-20px);
+      }
+
+      100% {
+        opacity: 1;
+        transform: translateX(0);
+      }
+    }
+  </style>
+</head>
+
+<body>
+  <link rel="stylesheet" href="css/clientstyle.css" />
+  <div>
+    <link href="css/about.css" rel="stylesheet" />
+
+    <div class="about-container">
+      <div class="about-header">
+        <header data-thq="thq-navbar" class="about-navbar-interactive">
+          <a href="index.php" class="about-logo">AllHere</a>
+          <div data-thq="thq-navbar-nav" class="about-desktop-menu">
+            <nav class="about-links">
+              <a href="index.php">Home</a>
+              <a href="service.php" class="about-nav2">Service</a>
+              
+              <a href="about.php" class="about-nav4">About</a>
+            </nav>
+            <div class="about-buttons">
+              <a href="login.php" class="about-login button">Login</a>
+              <a href="register.php" class="about-register button">
+                Register
+              </a>
+            </div>
+          </div>
+          <div data-thq="thq-burger-menu" class="about-burger-menu">
+            <svg viewBox="0 0 1024 1024" class="about-icon">
+              <path
+                d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z">
+              </path>
+            </svg>
+          </div>
+          <div data-thq="thq-mobile-menu" class="about-mobile-menu">
+            <div class="about-nav">
+              <div class="about-top">
+                <span class="about-logo1">COMPANY</span>
+                <div data-thq="thq-close-menu" class="about-close-menu">
+                  <svg viewBox="0 0 1024 1024" class="about-icon02">
+                    <path
+                      d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z">
+                    </path>
+                  </svg>
+                </div>
+              </div>
+              <nav class="about-links1">
+                <span class="about-nav11">Home</span>
+                <span class="about-nav21">Service</span>
+                <span class="about-nav41">About</span>
+                <span class="about-nav5">Login</span>
+              </nav>
+              <div class="about-buttons1">
+                <button class="about-login1 button">Login</button>
+                <button class="about-register1 button">Register</button>
+              </div>
+            </div>
+            <div>
+              <svg viewBox="0 0 950.8571428571428 1024" class="about-icon04">
+                <path
+                  d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z">
+                </path>
+              </svg><svg viewBox="0 0 877.7142857142857 1024" class="about-icon06">
+                <path
+                  d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z">
+                </path>
+              </svg><svg viewBox="0 0 602.2582857142856 1024" class="about-icon08">
+                <path
+                  d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z">
+                </path>
+              </svg>
+            </div>
+          </div>
+        </header>
+      </div>
+      <div class="about-details">
+        <div class="about-details1">
+          <div class="about-container1">
+            <span class="about-text sectionTitle">
+              <span>Details</span>
+              <br />
+            </span>
+            <h2 class="about-details-heading heading2">About Our Company</h2>
+            <span class="about-details-sub-heading">
+            All Here is a Cloud-Based Multi-Tenant Inventory System. This comprehensive solution, featuring a singular, all-encompassing plan, 
+            excels in simplifying inventory management for businesses. Users can effortlessly handle product, category, company, and customer information, ensuring a 
+            streamlined approach to inventory control. With robust transaction tracking and detailed transaction records, businesses gain insights into their operations. 
+            The system's user management ensures secure access, while its singular plan approach eliminates complexity. Cheers to a year of success in delivering efficiency 
+            and transparency to businesses seeking a seamless and inclusive inventory management solution!
+            </span>
+          </div>
+          <img alt="image"
+            src="https://images.unsplash.com/photo-1523217582562-09d0def993a6?crop=entropy&amp;cs=tinysrgb&amp;fit=max&amp;fm=jpg&amp;ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcwMDI3NzMxNXw&amp;ixlib=rb-4.0.3&amp;q=80&amp;w=400"
+            class="about-details-image" />
+        </div>
+      </div>
+      <div class="about-footer">
+        <footer class="about-footer1">
+          <div class="about-container2">
+            <span class="about-logo2">AllHere</span>
+            <nav class="about-nav1">
+              <a href="index.php" class="about-nav12">Home</a>
+              <a href="service.php" class="about-nav22">Service</a>
+              <a href="about.php" class="about-nav42">About</a>
+            </nav>
+          </div>
+          <div class="about-separator"></div>
+          <div class="about-container3">
+            <span class="about-text3">
+              © 2023 AllHere, All Rights Reserved.
+            </span>
+            <div class="about-icon-group1"></div>
+          </div>
+        </footer>
+      </div>
+    </div>
+  </div>
+  <script data-section-id="navbar" src="https://unpkg.com/@teleporthq/teleport-custom-scripts"></script>
+</body>
+
+</html>
